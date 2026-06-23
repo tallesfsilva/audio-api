9a0c6b2f3de0e4250bb3e8280638a5ec765ab268
Talles Fernando Silva <talles.silva@gmail.com>
2026-06-23 17:24:25 -0300

    Ajuste config/index

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,9 @@
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
  
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
